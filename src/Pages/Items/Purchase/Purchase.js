7071ed3e4a5867c13a0a10b91876bb28721153e1
Md. Rasheduzzaman Rajon <mrrajon76@gmail.com>
2022-06-02 21:15:45 +0600

    Prevent admin to purchase any product

--- src/Pages/Items/Purchase/Purchase.js
+++ src/Pages/Items/Purchase/Purchase.js
@@ -1,14 +1,27 @@
 import React from 'react';
-// import { useAuthState } from 'react-firebase-hooks/auth';
-// import { useNavigate } from 'react-router-dom';
-// import swal from 'sweetalert';
-// import auth from '../../../firebase.init';
-// import useUserStatus from '../../../hooks/useUserStatus';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import swal from 'sweetalert';
+import auth from '../../../firebase.init';
+import useUserStatus from '../../../hooks/useUserStatus';
 import Footer from '../../Shared/Footer/Footer';
 import Header from '../../Shared/Header/Header';
-// import Loading from '../../Shared/Loading/Loading';
+import Loading from '../../Shared/Loading/Loading';
 
 const Purchase = () => {
+    const [user] = useAuthState(auth);
+    const [admin, isLoading] = useUserStatus(user);
+    const navigate = useNavigate();
+
+    if (isLoading) {
+        return <Loading />;
+    }
+    if (admin) {
+        swal('Admin can not purchase any product', {
+            icon: "error"
+        });
+        navigate('/');
+    }
 
     return (
         <div>
